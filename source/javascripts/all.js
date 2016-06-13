4c546c52e9d0aa2f35b15dac13ac6e7c425f47d7
Omar Sahyoun <osahyoun@gmail.com>
2016-06-13 17:15:10 +0100

    Home with feed

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,21 @@
-// This is where it all goes :)
+var $actions = $('.actions');
+
+var template = _.template(
+  $('script.template').html()
+);
+
+var socket = io('http://live-actions-production.cwctan5wfu.us-west-2.elasticbeanstalk.com' );
+
+socket.on('actions', function(msg){
+  var data = JSON.parse(msg);
+
+  var actionHtml = $(template(data));
+
+  $actions.prepend(actionHtml);
+
+  actionHtml.fadeIn(function(){
+    $('.card:gt(50)').remove();
+  });
+});
+
+
