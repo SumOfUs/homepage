ee72b0c5ecc5c0b5a25f80f30450811b5c7a403d
Neal Donnelly <NealJMD@gmail.com>
2016-06-16 13:36:09 -0600

    get i18n-js working

--- source/javascripts/homepage.js
+++ source/javascripts/homepage.js
@@ -1,9 +1,7 @@
 window.$ = require('jquery');
 window._ = require('underscore');
 window.Backbone = require('backbone');
-
-////= require i18n
-////= require i18n/translations
+window.I18n = require('i18n-js');
 
 window.ActionStream  = require('./sumofus/backbone/action_stream');
 window.PoiMap        = require('./sumofus/backbone/poi_map');
