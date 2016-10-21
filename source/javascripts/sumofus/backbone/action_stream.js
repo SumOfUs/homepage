48238bd1535dd5d192f09f09eb3ee68ac0f0be17
Neal Donnelly <NealJMD@gmail.com>
2016-10-21 16:38:45 -0700

    make action stream slower, clickable, and fit in teal area

--- source/javascripts/sumofus/backbone/action_stream.js
+++ source/javascripts/sumofus/backbone/action_stream.js
@@ -2,12 +2,16 @@ const ActionStream = Backbone.View.extend({
 
   el: '.action-stream',
   ACTION_SOURCE: '//live-actions-production.cwctan5wfu.us-west-2.elasticbeanstalk.com',
-  ANIMATION_SECONDS: 0.8,
+  LINK_ROOT: 'https://actions.sumofus.org',
+  MAX_PER_PERIOD: 1,
+  INSERTION_PERIOD: 1.2, // seconds
+  ANIMATION_DURATION: 0.8, // seconds
+
   MAX_ACTION_ELS: 20,
 
   initialize() {
     window.setInterval(this.updateTimestamps.bind(this), 1000);
-    window.setInterval(this.renderActions.bind(this), this.ANIMATION_SECONDS*1000);
+    window.setInterval(this.renderActions.bind(this), this.INSERTION_PERIOD*1000);
     this.actionsContainer = this.$('.action-stream__actions');
     this.actionQueue = [];
     this.socket = io(this.ACTION_SOURCE);
@@ -40,20 +44,21 @@ const ActionStream = Backbone.View.extend({
   },
 
   // rather than render the actions as they arrive, we batch them and add
-  // them every ANIMATION_SECONDS because the CSS transition gets stopped
+  // them every INSERTION_PERIOD because the CSS transition gets stopped
   // when we add new actions.
   renderActions() {
     if(this.actionQueue.length === 0) { return; }
 
     // add the elements to the DOM
     let totalWidth = 0
-    for (var ii = 0; ii < this.actionQueue.length; ii++) {
-      let action = this.actionQueue[ii];
+    let countToAdd = this.MAX_PER_PERIOD || this.actionQueue.length;
+    for (var ii = 0; ii < countToAdd; ii++) {
+      let action = this.actionQueue.shift();
       let name = this.formatName(action.first_name, action.last_name);
-      this.actionsContainer.prepend(this.template(action.type, name, 1));
+      let url = this.LINK_ROOT + action.uri;
+      this.actionsContainer.prepend(this.template(action.type, name, 1, url));
       totalWidth += this.actionsContainer.children().first().outerWidth();
     }
-    this.actionQueue = [];
 
     // prune excessive elements no longer on screen
     this.actionsContainer.children().slice(this.MAX_ACTION_ELS).remove();
@@ -62,19 +67,19 @@ const ActionStream = Backbone.View.extend({
     this.actionsContainer.css('transition', 'left 0s');
     this.actionsContainer.css('left', `-${totalWidth}px`);
     window.setTimeout(()=> {
-      this.actionsContainer.css('transition', `left ${this.ANIMATION_SECONDS}s ease-in-out`);
+      this.actionsContainer.css('transition', `left ${this.ANIMATION_DURATION}s ease-in-out`);
       this.actionsContainer.css('left', '0');
     }, 32);
   },
 
-  template(action_type, name, seconds_ago) {
+  template(action_type, name, seconds_ago, url) {
     if(action_type !== 'petition' && action_type !== 'donation') {
       console.error(`Invalid action_type '${action_type}'. Expected 'petition' or 'donation'.`);
       return;
     }
     let actionTranslation = `homepage.action_stream.${action_type}`
     let icon = (action_type === 'donation') ? 'usd' : 'pencil'
-    return `<div class="action-stream__template stream-action">
+    return `<a class="action-stream__template stream-action" href="${url}">
               <div class="stream-action__icon-holder">
                 <span class="stream-action__icon fa fa-${icon}"></span>
               </div>
@@ -86,7 +91,7 @@ const ActionStream = Backbone.View.extend({
                   ${I18n.t('homepage.action_stream.seconds_ago', {seconds: seconds_ago})}
                 </div>
               </div>
-            </div>`
+            </a>`
   },
 
 });
