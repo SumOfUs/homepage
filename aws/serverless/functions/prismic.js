cc826a0a0cd96bb314e4d22c2aa41f44cff57ab8
Omar Sahyoun <osahyoun@gmail.com>
2017-09-13 11:51:34 +0100

    Trigger deployment

--- aws/serverless/functions/prismic.js
+++ aws/serverless/functions/prismic.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const http = require('request');
+
 module.exports.handler = (event, context, callback) => {
-  console.log(JSON.stringify(event));
+  const body = JSON.parse(event.body);
+  console.log(JSON.stringify(body));
+  const token = process.env.CI_TOKEN;
+  const url = `https://circleci.com/api/v1.1/project/github/SumOfUs/homepage/tree/prismic?circle-token=${token}`;
+
+
+  http
+  .post(url, (err, response, body) => {
+    if(err){
+      console.log('error:', err);
+    }
+
+    console.log(body);
+  });
 
   const response = {
     statusCode: 200,
