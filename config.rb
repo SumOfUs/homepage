8bdd1afc91a1c3034a1c4ee600f247a6b6b1e4b8
Rodrigo Pavano <rodrigopavano@gmail.com>
2018-01-09 17:52:57 -0300

    Updated dev config

--- config.rb
+++ config.rb
@@ -20,7 +20,7 @@ page '/*.txt', layout: false
 
 # Reload the browser automatically whenever files change
 configure :development do
-  config[:api_host] = "//localhost:3000"
+  config[:api_host] = "//localhost:4567"
   activate :livereload
 end
 
