c34e5b8a242001ab1d25597be20b46aeedba273b
saraf22 <sarafathulla@f22labs.com>
2019-09-16 11:57:10 +0530

    privacy policy bug fixes

--- source/javascripts/privacy_policy.js
+++ source/javascripts/privacy_policy.js
@@ -1,20 +1,24 @@
-var links = document.querySelectorAll("a");
-var accordianHeaders = document.querySelectorAll(".privacy__question");
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", e => {
-    changeClass(e.target.hash.replace("#", "."));
-  });
+function internalNav(link, pLink) {
+changeClass(link, pLink)
 }
+var accordianHeaders = document.querySelectorAll(".privacy__question");
 for (var i = 0; i < accordianHeaders.length; i++) {
   accordianHeaders[i].addEventListener("click", e => {
     toggleFunction(e.target.parentElement.classList[0]);
   });
 }
-function changeClass(link) {
-  var p = document.querySelector(`${link} .privacy__answer`);
-  var h2 = document.querySelector(`${link} h2`);
+function changeClass(link, pLink) {
+  var replacedHash;
+  if(pLink) {
+    replacedHash = pLink.replace("#", ".");
+  } else {
+    replacedHash = link.replace("#", ".");
+  }
+  var p = document.querySelector(`${replacedHash} .privacy__answer`);
+  var h2 = document.querySelector(`${replacedHash} h2`);
   p.classList.remove("hidden-closed");
   h2.classList.add("privacy__question--open");
+  window.location.href = link;
 }
 function toggleFunction(class_) {
   var p = document.querySelector(`.${class_} .privacy__answer`);
