aff4cc118426467479334e1f5908302bcecfd5d4
subbiah <subbiah@f22labs.com>
2019-09-20 12:21:38 +0530

    Campaigns % achieved

--- source/javascripts/sumofus/backbone/campaigns.js
+++ source/javascripts/sumofus/backbone/campaigns.js
@@ -1,69 +1,79 @@
 const Campaigns = Backbone.View.extend({
+  el: ".campaign-tiles",
 
-  el: '.campaign-tiles',
-
-  JSON_PATH: '/api/pages/featured.json',
+  JSON_PATH: "/api/pages/featured.json",
   FADE_IN_SECONDS: 0.8,
   BAR_FILL_SECONDS: 0.4,
   MIN_ACTION_COUNT: 1000,
 
   initialize(options) {
-    this.apiHost = options.apiHost || '';
+    this.apiHost = options.apiHost || "";
     this.loadCampaigns(options.language);
     this.source = options.source;
     this.limit = options.limit || -1;
   },
 
-  loadCampaigns(language='en') {
+  loadCampaigns(language = "en") {
     $.get(
-      this.apiHost + this.JSON_PATH, {language: language}, this.success.bind(this)
+      this.apiHost + this.JSON_PATH,
+      { language: language },
+      this.success.bind(this)
     ).fail(this.failure.bind(this));
   },
 
   success(data) {
-    $('.campaign-list__loading').addClass('hidden-irrelevant');
+    $(".campaign-list__loading").addClass("hidden-irrelevant");
     for (var ii = 0; ii < data.length; ii++) {
       if (ii >= this.limit && this.limit !== -1) break;
-      this.$el.append(this.template(data[ii].title,
-                                    this.addSource(data[ii].url),
-                                    data[ii].image,
-                                    data[ii].campaign_action_count || data[ii].action_count));
+      this.$el.append(
+        this.template(
+          data[ii].title,
+          this.addSource(data[ii].url),
+          data[ii].image,
+          data[ii].campaign_action_count || data[ii].action_count
+        )
+      );
     }
     window.setTimeout(() => {
-      this.$('.campaign-tile').removeClass('transparent');
-      $('.campaign-tiles--empty').removeClass('campaign-tiles--empty');
+      this.$(".campaign-tile").removeClass("transparent");
+      $(".campaign-tiles--empty").removeClass("campaign-tiles--empty");
     }, 100);
   },
 
   addSource(url) {
     if (!this.source) return url;
-    return this.addParam(url, 'source', this.source);
+    return this.addParam(url, "source", this.source);
   },
 
   addParam(url, key, value) {
-    let hashSplit = url.split('#');
-    let paramStarter = (hashSplit[0].indexOf('?') > -1) ? '&' : '?';
+    let hashSplit = url.split("#");
+    let paramStarter = hashSplit[0].indexOf("?") > -1 ? "&" : "?";
     let output = `${hashSplit[0]}${paramStarter}${key}=${value}`;
     if (hashSplit.length > 1) {
-      output = `${output}#${hashSplit[1]}`
+      output = `${output}#${hashSplit[1]}`;
     }
-    return output
+    return output;
   },
 
   failure(e) {
-    $('.campaign-list__loading').addClass('hidden-irrelevant');
-    $('.campaign-list__failed').removeClass('hidden-irrelevant');
-    $('.campaign-tiles--empty').removeClass('campaign-tiles--empty');
+    $(".campaign-list__loading").addClass("hidden-irrelevant");
+    $(".campaign-list__failed").removeClass("hidden-irrelevant");
+    $(".campaign-tiles--empty").removeClass("campaign-tiles--empty");
   },
 
   template(title, pageUrl, imageUrl, actionCount) {
     if (imageUrl.length) {
       var backgroundStyle = `background-image: url(${imageUrl})`;
-    } 
-    const completedAction = actionCount > 100 ? `width: ${actionCount.toString().substr(0, 2)}` : `width: ${actionCount}%`;
+    }
+    const completedAction =
+      actionCount > 100
+        ? `width: ${actionCount.substr(0, 2)}%`
+        : `width: ${actionCount}%`;
     var overlay = `<div class="campaign-tile__overlay">
-                    ${I18n.t('pages.campaigns.action_count', {count: I18n.toNumber(actionCount, {precision: 0})})}
-                  </div>`
+                    ${I18n.t("pages.campaigns.action_count", {
+                      count: I18n.toNumber(actionCount, { precision: 0 })
+                    })}
+                  </div>`;
     return `<div class="campaign-container">
               <a class="campaign-tile campaign-tile--compact transparent" href="${pageUrl}">
                 <div class="campaign-tile__image"
@@ -73,23 +83,22 @@ const Campaigns = Backbone.View.extend({
                 <div class="campaign-tile__action-bar">
                   <div class="campaign-tile__completed-action-bar" style="${completedAction}"></div>
                 </div>
-                <div class="campaign-tile__action-count">${ actionCount } Achieved</div>
+                <div class="campaign-tile__action-count">${completedAction}% Achieved</div>
                 <div class="campaign-tile__cta campaign-tile__open-cta">
-                  ${I18n.t('homepage.cta.take_action')}
+                  ${I18n.t("homepage.cta.take_action")}
                 </div>
               </a>
             </div>`;
   },
 
   // from http://stackoverflow.com/questions/11120840/hash-string-into-rgb-color
-  hashString(str){
+  hashString(str) {
     var hash = 5381;
     for (let ii = 0; ii < str.length; ii++) {
-      hash = ((hash << 5) + hash) + str.charCodeAt(ii); /* hash * 33 + c */
+      hash = (hash << 5) + hash + str.charCodeAt(ii); /* hash * 33 + c */
     }
     return hash;
-  },
-
+  }
 });
 
 module.exports = Campaigns;
