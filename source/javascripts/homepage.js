7deaaadc7e2e9fb8e4fb5639affba9df8e870f7a
Vince Martinez <eykosioux@gmail.com>
2019-10-11 10:52:26 +0100

    Greenhouse work in progress

--- source/javascripts/homepage.js
+++ source/javascripts/homepage.js
@@ -7,14 +7,14 @@ window.Stickyfill = require('stickyfill');
 require('selectize');
 require('./slidey_header');
 
-window.Faq           = require('./sumofus/backbone/faq');
-window.ActionStream  = require('./sumofus/backbone/action_stream');
-window.PoiMap        = require('./sumofus/backbone/poi_map');
+window.Faq = require('./sumofus/backbone/faq');
+window.ActionStream = require('./sumofus/backbone/action_stream');
+window.PoiMap = require('./sumofus/backbone/poi_map');
 window.PressCarousel = require('./sumofus/backbone/press_carousel');
-window.SignupForm    = require('./sumofus/backbone/signup_form');
-window.Campaigns     = require('./sumofus/backbone/campaigns');
+window.SignupForm = require('./sumofus/backbone/signup_form');
+window.Campaigns = require('./sumofus/backbone/campaigns');
 window.UnsubscribeForm = require('./sumofus/backbone/unsubscribe_form');
 window.BankingDetailsForm = require('./sumofus/backbone/banking_details_form');
 window.SweetPlaceholder = require('./sumofus/backbone/sweet_placeholder');
 window.FundingTable = require('./sumofus/backbone/funding/funding_table');
-window.ExpensesTable = require('./sumofus/backbone/funding/expenses_table')
+window.ExpensesTable = require('./sumofus/backbone/funding/expenses_table');
