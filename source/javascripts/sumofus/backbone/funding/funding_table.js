fe7983c2c6b1e7f3f4058dfda66ec536e68484ec
subbiah <subbiah@f22labs.com>
2019-12-13 14:25:57 +0530

    Changes to data in funding page

--- source/javascripts/sumofus/backbone/funding/funding_table.js
+++ source/javascripts/sumofus/backbone/funding/funding_table.js
@@ -3,25 +3,25 @@ const TableTemplate = require('./table_template.js');
 
 const Funding = Backbone.Model.extend({
   defaults: {
-    _2015: {
-      total: 4857359,
-      individuals: 4080182,
-      foundations: 761250,
-      other: 15927
-    },
     _2016: {
       total: 4637815,
       individuals: 3869976,
       foundations: 612728,
-      other: 155111
+      other: 155111,
     },
     _2017: {
       total: 5659222,
       individuals: 5272378,
       foundations: 190035,
-      other: 196809
-    }
-  }
+      other: 196809,
+    },
+    _2018: {
+      total: 5614818,
+      individuals: 4858584,
+      foundations: 739814,
+      other: 16420,
+    },
+  },
 });
 
 const FundingTable = Backbone.View.extend({
@@ -37,23 +37,32 @@ const FundingTable = Backbone.View.extend({
 
   initialize: function() {
     // render initial format - default to USD
-    this.$el.html(this.template('funding', this.categories, this.model.attributes, 'USD'));
+    this.$el.html(
+      this.template('funding', this.categories, this.model.attributes, 'USD')
+    );
     return this;
   },
 
   changeCurrency(e) {
     console.log(e);
-    const currency = $('#funding-currency-select option:selected').val()
+    const currency = $('#funding-currency-select option:selected').val();
     const rates = this.conversionRates[currency];
     var newAttributes = {};
-    _.each(this.model.attributes, function(sources, year){
+    _.each(this.model.attributes, function(sources, year) {
       newAttributes[year] = {};
-      _.each(sources, function(amount, source){
-        newAttributes[year][source] = Math.round(amount / rates[year])
+      _.each(sources, function(amount, source) {
+        newAttributes[year][source] = Math.round(amount / rates[year]);
       });
     });
     const convertedModel = new Funding(newAttributes);
-    this.$el.html(this.template('funding', this.categories, convertedModel.attributes, currency));
+    this.$el.html(
+      this.template(
+        'funding',
+        this.categories,
+        convertedModel.attributes,
+        currency
+      )
+    );
     return this;
   },
 
