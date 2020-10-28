8a917739ff090c103e7b78b03977b1d0145fde2e
subbiah <subbiah@f22labs.com>
2020-10-28 18:24:45 +0530

    Update endpoint to point STG

--- config.rb
+++ config.rb
@@ -26,8 +26,8 @@ end
 
 # Build-specific configuration
 configure :build do
-  config[:api_host] = "https://actions.sumofus.org"
-  #config[:api_host] = "https://action-staging.sumofus.org"
+  # config[:api_host] = "https://actions.sumofus.org"
+  config[:api_host] = "https://action-staging.sumofus.org"
   activate :gzip
 end
 
