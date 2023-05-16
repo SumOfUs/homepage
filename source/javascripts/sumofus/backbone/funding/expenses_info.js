513e3496213fa189b8243126e6ccb4bec3f662f2
Subbiah <49471498+subbiahsn@users.noreply.github.com>
2023-05-16 17:08:17 +0530

    Funding migration (#295)
    * Migrate funding page to prismic
    
    * Update locale files
    
    * Fixes style issues
    
    * Reverts lock file
    
    * Fixes style issues

---
+++ source/javascripts/sumofus/backbone/funding/expenses_info.js
@@ -0,0 +1,4 @@
+const expenses = {"_2019":{"total":5785935,"campaigns":4426757,"ops":861921,"fundraising":497257},"_2020":{"total":6605040,"campaigns":5182736,"ops":864561,"fundraising":557743},"_2021":{"total":8481977,"campaigns":6473653,"ops":1093845,"fundraising":914479}};
+
+
+module.exports = expenses;
--- source/javascripts/sumofus/backbone/funding/funding_table.js
+++ source/javascripts/sumofus/backbone/funding/funding_table.js
@@ -1,45 +1,9 @@
 const conversionRates = require('./conversion_rates.js');
+const fundingInfo = require('./funding_info.js');
 const TableTemplate = require('./table_template.js');
 
 const Funding = Backbone.Model.extend({
-  defaults: {
-    _2016: {
-      total: 4637815,
-      individuals: 3869976,
-      foundations: 612728,
-      other: 155111,
-    },
-    _2017: {
-      total: 5659222,
-      individuals: 5272378,
-      foundations: 190035,
-      other: 196809,
-    },
-    _2018: {
-      total: 5614818,
-      individuals: 4858584,
-      foundations: 739814,
-      other: 16420,
-    },
-    _2019: {
-      total: 5788722,
-      individuals: 5339913,
-      foundations: 347440,
-      other: 101369,
-    },
-    _2020: {
-      total: 7110840,
-      individuals: 5604251,
-      foundations: 1501115,
-      other: 5474,
-    },
-    _2021: {
-      total: 9300233,
-      individuals: 7297046,
-      foundations: 1982370,
-      other: 20817,
-    },
-  },
+  defaults: fundingInfo,
 });
 
 const FundingTable = Backbone.View.extend({
